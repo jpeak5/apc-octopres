97b9658de41511fcf0394d1e87f21c552dd2ae3d
jason peak <jpeak5@lsu.edu>
2013-05-07 15:14:55 -0500

    auto-commit

--- auto/bib.el
+++ auto/bib.el
@@ -3,5 +3,6 @@
     (LaTeX-add-bibitems
      "gartner_mets_2012"
      "tei_sig_on_libraries_best_2011"
-     "lisa_spiro_archival_2009")))
+     "lisa_spiro_archival_2009"
+     "dow_elizabeth_2009")))
 
