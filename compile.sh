ec3219c34e8e197a366382e04b6916e6c202f21c
jason peak <jpeak5@lsu.edu>
2013-05-07 10:36:59 -0500

    adds ignore and compile script

---
+++ compile.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+latex apc.tex
+bibtex apc.aux
+latex apc.tex
+latex apc.tex
+latex2html -html_version 4.1 --no-navigation -split +0 -unsegment -info=0 apc.tex
+
+latex lis.tex
+bibtex lis.aux
+latex lis.tex
+latex lis.tex
+latex2html -html_version 4.1 --no-navigation -split +0 -unsegment -info=0 lis.tex
