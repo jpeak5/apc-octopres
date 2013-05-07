cffbb69479b2fb7448c90db64b20f16a3206ce2d
jason peak <jpeak5@lsu.edu>
2013-05-07 09:43:39 -0500

    adding everything uncommitted

---
+++ draft/labels.pl
@@ -0,0 +1,17 @@
+# LaTeX2HTML 2008 (1.71)
+# Associate labels original text with physical files.
+
+
+$key = q/cite_tei_sig_on_libraries_best_2011/;
+$external_labels{$key} = "$URL/" . q|draft.html|; 
+$noresave{$key} = "$nosave";
+
+1;
+
+
+# LaTeX2HTML 2008 (1.71)
+# labels from external_latex_labels array.
+
+
+1;
+
