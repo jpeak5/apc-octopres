8f941819c78ec2b9d9d39aa98f61a4f533493555
jason peak <jpeak5@lsu.edu>
2013-05-07 10:32:34 -0500

    adding separate document, etc for the APC group effort

---
+++ apc/internals.pl
@@ -0,0 +1,10 @@
+# LaTeX2HTML 2008 (1.71)
+# Associate internals original text with physical files.
+
+
+$key = q/cite_tei_sig_on_libraries_best_2011/;
+$ref_files{$key} = "$dir".q|apc.html|; 
+$noresave{$key} = "$nosave";
+
+1;
+
